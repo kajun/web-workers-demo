d8950da081e1f9758005a5765188064612cc4106
Jun Clemente <kjstudios858@gmail.com>
2016-02-28 17:47:01 -0800

    refactored to use webworker

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -40,23 +40,36 @@
 
     toggleButtonsAbledness();
 
+    // type "python -m http.server" to use webworkers locally
     // Hint! This is where you should post messages to the web worker and
     // receive messages from the web worker.
 
-    length = imageData.data.length / 4;
-    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-      r = imageData.data[i * 4 + 0];
-      g = imageData.data[i * 4 + 1];
-      b = imageData.data[i * 4 + 2];
-      a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
-      imageData.data[i * 4 + 0] = pixel[0];
-      imageData.data[i * 4 + 1] = pixel[1];
-      imageData.data[i * 4 + 2] = pixel[2];
-      imageData.data[i * 4 + 3] = pixel[3];
+    if (window.Worker) {
+      var myWorker = new Worker("../scripts/worker.js");
+      myWorker.postMessage({"imageData": imageData,
+                          "type": type});
+      console.log("msg posted to webworker");
     }
-    toggleButtonsAbledness();
-    return ctx.putImageData(imageData, 0, 0);
+
+    // length = imageData.data.length / 4;
+    // for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
+    //   r = imageData.data[i * 4 + 0];
+    //   g = imageData.data[i * 4 + 1];
+    //   b = imageData.data[i * 4 + 2];
+    //   a = imageData.data[i * 4 + 3];
+    //   pixel = manipulate(type, r, g, b, a);
+    //   imageData.data[i * 4 + 0] = pixel[0];
+    //   imageData.data[i * 4 + 1] = pixel[1];
+    //   imageData.data[i * 4 + 2] = pixel[2];
+    //   imageData.data[i * 4 + 3] = pixel[3];
+    // }
+
+    myWorker.onmessage = function(e) {
+      imageData = e.data;
+      console.log("msg received from webworker");
+      toggleButtonsAbledness();
+      return ctx.putImageData(imageData, 0, 0);
+    }
   };
 
   function revertImage() {
